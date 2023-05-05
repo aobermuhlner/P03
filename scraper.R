bcffa1498d5f81af3541eac6eca6f544ef999e01
Linus Stuhlmann <stuhllin@students.zhaw.ch>
2023-05-05 09:51:21 +0200

    Update scraper.R

--- scraper.R
+++ scraper.R
@@ -1,2 +1,12 @@
-install.packages("data.table")
-library(data.table)
+library(rvest)
+
+
+url <- "https://fis.fda.gov/extensions/FPD-QDE-FAERS/FPD-QDE-FAERS.html"
+
+# HTML-Elemente mit den Links finden und extrahieren
+links <- read_html(url) %>% 
+  html_nodes(xpath = '//td[@class="fpd-table"]/a') %>% 
+  html_attr("href")
+
+# Ausgabe der Links
+print(links)
