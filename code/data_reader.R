716f2608ce7ee0eb1de502b14e083a634723a2a5
Obermühlner Adrian (obeadr02) <obeadr02@students.zhaw.ch>
2023-05-15 15:57:14 +0200

    fix hstogram

--- code/data_reader.R
+++ code/data_reader.R
@@ -23,47 +23,40 @@ load_data <- function(entity, years) {
 entities = c('THER', 'RPSR', 'REAC', 'OUTC', 'INDI', 'DRUG', 'DEMO')
 years = c('22','23')
 
-THER <- load_data("THER", years)
-OUTC <- load_data("OUTC", years)
-INDI <- load_data("INDI", years)
-DRUG <- load_data("DRUG", years)
-DEMO <- load_data("DEMO", years)
-
-REAC <- load_data("REAC", years)
-RPSR <- load_data("RPSR", years)
-
-data_tables <- list(THER, RPSR, OUTC, INDI, DRUG, DEMO)
+for (entity in entities) {
+  assign(entity, load_data(entity, years))
+}
+##############
 
 setnames(THER, "dsg_drug_seq", "drug_seq") 
 setnames(INDI, "indi_drug_seq", "drug_seq")
-
 
 ########################################
 
 join_data <- function(v_drugname){
   
   # Create a data table with the selected_drug details
-  selected_drug <- unique(DRUG[drugname == v_drugname, .(primaryid, caseid, drug_seq, drugname, route, YearQuarter)])
+  selected_drug <- unique(DRUG[drugname == v_drugname, .(primaryid,caseid, drug_seq, drugname, route, YearQuarter)])
   
   # Create a data table with the selected_patients details
-  selected_patients <- unique(DEMO[primaryid %in% selected_drug$primaryid, .(primaryid, caseid,age,sex,wt, reporter_country)])
+  selected_patients <- unique(DEMO[primaryid %in% selected_drug$primaryid, .(primaryid,age,sex,wt, reporter_country)])
   
   # Create a data table with the selected_therapies details
-  selected_therapies <- unique(THER[primaryid %in% selected_drug$primaryid, .(primaryid, caseid, drug_seq, start_dt, end_dt, dur)])
+  selected_therapies <- unique(THER[primaryid %in% selected_drug$primaryid, .(primaryid, drug_seq, start_dt, end_dt, dur)])
   
   # Create a data table with the selected_indications details
-  selected_indications <- unique(INDI[primaryid %in% selected_drug$primaryid])
+  selected_indications <- unique(INDI[primaryid %in% selected_drug$primaryid, .(primaryid, drug_seq, indi_pt)])
   
   # Create a data table with the selected_outcomes details
-  selected_outcomes <- OUTC[primaryid %in% selected_drug$primaryid, .(primaryid, caseid, outc_cod)]
-  selected_outcomes <- selected_outcomes[selected_outcomes[, .I[which.max(.I)], by = .(primaryid, caseid)]$V1]
+  selected_outcomes <- OUTC[primaryid %in% selected_drug$primaryid, .(primaryid, outc_cod)]
+  selected_outcomes <- selected_outcomes[selected_outcomes[, .I[which.max(.I)], by = .(primaryid)]$V1]
   
   
   # Merge all the data tables
-  final_data <- merge(selected_drug, selected_outcomes, by = c("primaryid","caseid"), all.x = TRUE)
-  final_data <- merge(final_data, selected_patients, by = c("primaryid","caseid"), all.x = TRUE)
-  final_data <- merge(final_data, selected_therapies, by = c("primaryid", "drug_seq","caseid"), all.x = TRUE)
-  final_data <- merge(final_data, selected_indications, by = c("primaryid", "drug_seq","caseid"), all.x = TRUE)
+  final_data <- merge(selected_drug, selected_outcomes, by = c("primaryid"), all.x = TRUE)
+  final_data <- merge(final_data, selected_patients, by = c("primaryid"), all.x = TRUE)
+  final_data <- merge(final_data, selected_therapies, by = c("primaryid", "drug_seq"), all.x = TRUE)
+  final_data <- merge(final_data, selected_indications, by = c("primaryid", "drug_seq"), all.x = TRUE)
   
   # Ensure only unique rows in the final data
   final_data <- unique(final_data)
@@ -72,7 +65,7 @@ join_data <- function(v_drugname){
 }
 
 # Run the function
-final_data <- join_data("LITHIUM")
+final_data <- join_data("IBUPROFEN")
 View(final_data)
 
 #Funktionen
@@ -85,7 +78,7 @@ unique_drugs
 #Liste aller beobachteten Sequenzen an Medikamenten
 
 
-#Statistiken
+#Funktionen für plots
 num_reports_per_quarter <- function(data){
   reports <- data[, .N, by = YearQuarter]
   return(reports)
@@ -96,9 +89,20 @@ num_reports_per_sequence <- function(data){
   return(reports)
 }
 
-hist_therapy_duration <- function(data){
-  hist_data <- hist(data$dur, plot = FALSE) 
-  return(hist_data) 
+calc_therapy_duration <- function(data){
+  ther_data <- data[!is.na(start_dt) & !is.na(end_dt), .(start_dt, end_dt)]
+  ther_data[, `:=`(start_dt = as.character(start_dt), end_dt = as.character(end_dt))]
+  ther_data[start_dt %like% "^\\d{6}$", start_dt := paste0(start_dt, "01")]
+  ther_data[end_dt %like% "^\\d{6}$", end_dt := paste0(end_dt, "01")]
+  
+  ther_data[, `:=`(start_dt = as.Date(start_dt, format = "%Y%m%d"),
+              end_dt = as.Date(end_dt, format = "%Y%m%d"))]
+  
+  durations <- as.numeric(ther_data$end_dt - ther_data$start_dt)
+  
+  durations[durations < 0] <- 0
+  
+  return(durations)
 }
 
 top_indications <- function(data){
@@ -115,7 +119,7 @@ outcome_distribution <- function(data){
 
 
 
-#Aufruf
+#erstellung plots
 library(ggplot2)
 
 # Plotting number of reports per quarter:
@@ -131,7 +135,8 @@ ggplot(reports_per_sequence, aes(x = factor(drug_seq), y = N)) +
   labs(title = "Number of Reports per Sequence", x = "Sequence", y = "Number of Reports")
 
 #Plotting histogram of therapy duration:
-hist_data <- hist_therapy_duration(final_data)
+therapy_durations <- calc_therapy_duration(final_data)
+hist(therapy_durations)
 ggplot(hist_data, aes(x = mids, y = counts)) +
   geom_bar(stat = "identity") +
   labs(title = "Distribution of Therapy Duration", x = "Duration", y = "Frequency")
@@ -148,7 +153,7 @@ ggplot(outcome_distribution_data, aes(x = outc_cod, y = N)) +
   geom_bar(stat = "identity") +
   labs(title = "Outcome Distribution", x = "Outcome Code", y = "Number of Outcomes")
 
-
+View(hist_data)
 
 
 
