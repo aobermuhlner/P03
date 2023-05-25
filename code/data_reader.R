a7927cce6f1cd1be817dbbd8b70364642e7dc0bd
Nguyen Minh-Khan (nguyemi1) <mnguyen@gmx.ch>
2023-05-25 19:27:33 +0200

    filter fix + remove column filter in data table visual

--- code/data_reader.R
+++ code/data_reader.R
@@ -33,26 +33,27 @@ join_data_drug <- function(v_drugname = NULL) {
   return(drug_data)
 }
 
-filter_data <- function(data, v_sex = NULL, v_age_min = NULL, v_age_max = NULL, v_year = NULL, v_sequenz = NULL) {
+filter_data <- function(data, v_sex = NULL, v_age_min = NULL, v_age_max = NULL, v_year = NULL, v_sequence_min = NULL, v_sequence_max = NULL) {
   
   v_age_min <- ifelse(is.null(v_age_min), min(data$age, na.rm = TRUE), v_age_min)
   v_age_max <- ifelse(is.null(v_age_max), max(data$age, na.rm = TRUE), v_age_max)
   
+  v_sequence_min <- ifelse(is.null(v_sequence_min), min(data$drug_seq, na.rm = TRUE), v_sequence_min)
+  v_sequence_max <- ifelse(is.null(v_sequence_max), max(data$drug_seq, na.rm = TRUE), v_sequence_max)
+  
   if (is.null(v_year) | v_year == "All"){
     v_year <- unique(data$year)
   }
-  if (is.null(v_sequenz) | v_sequenz == "All"){
-    v_sequenz <- unique(data$drug_seq)
-  }
+  
   if (is.null(v_sex) | v_sex == "All") {
-    filtered_data <- data[age >= v_age_min & age <= v_age_max,]
+    filtered_data <- data[age >= v_age_min & age <= v_age_max & drug_seq >= v_sequence_min & drug_seq <= v_sequence_max,]
   } else {
-    filtered_data <- data[sex %in% v_sex & age >= v_age_min,]
+    filtered_data <- data[sex %in% v_sex & age >= v_age_min & age <= v_age_max & drug_seq >= v_sequence_min & drug_seq <= v_sequence_max,]
   }
-  filtered_data <- data[drug_seq %in% v_sequenz & year %in% v_year]
+  
+  filtered_data <- filtered_data[year %in% v_year]
   return(filtered_data)
 }
-
 
 ########################################## Testing
 # Run the function
